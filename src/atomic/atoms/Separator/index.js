bb92f332db007fc90108857986b9e180630ddb23
Rafael Chianca <rafachianca@gmail.com>
2021-02-28 23:53:39 -0300

    Implements separator bar

---
+++ src/atomic/atoms/Separator/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SeparatorLine } from './styles';
+
+export default function Separator({ ...rest }) {
+  return (
+    <SeparatorLine {...rest} />
+  );
+}
---
+++ src/atomic/atoms/Separator/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+
+export const SeparatorLine = styled.View`
+    height: 1px;
+    width: 100%;
+    background-color: #bdbdbd;
+`;
