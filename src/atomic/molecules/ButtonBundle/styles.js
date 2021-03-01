155d3267a242f620d1625374a138a13d60b7724b
Rafael Chianca <rafachianca@gmail.com>
2021-02-28 23:54:02 -0300

    Removes mock components

--- src/atomic/molecules/ButtonBundle/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/native';
-
-export const MoleculeContainer = styled.View`
-    background-color: yellow;
-    padding: 20px;
-    align-items: center;
-    justify-content: center;
-`;
--- src/atomic/organisms/BundleFrame/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/native';
-
-export const OrganismContainer = styled.View`
-    background-color: red;
-    padding: 20px;
-    align-items: center;
-    justify-content: center;
-`;
