e0399e51a875bc6d6a7ec40d8cc1a06ae10d3106
Rafael Chianca <rafachianca@gmail.com>
2021-03-15 21:24:48 -0300

    Implements round profile picture atom

---
+++ src/atomic/atoms/ProfilePicture/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    height: 80px;
+    width: 80px;
+    border-radius: 500px;
+    background-color: #f1f2f2;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Picture = styled.ImageBackground`
+    height: 80px;
+    width: 80px;
+    border-radius: 500px;
+`;
