ed2a2de4f5d3f3672f01fef5bfde8eed96e797da
Rafael Chianca <rafachianca@gmail.com>
2021-04-01 20:47:10 -0300

    Style changes

--- src/services/imageRelated.js
+++ src/services/imageRelated.js
@@ -1,14 +1,14 @@
 import { Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
-if (Platform.OS !== 'web') {
-    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (status !== 'granted') {
-      alert('Sorry, we need camera roll permissions to make this work!');
-    }
-}
+// if (Platform.OS !== 'web') {
+//     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+//     if (status !== 'granted') {
+//       alert('Sorry, we need camera roll permissions to make this work!');
+//     }
+// }
 
-export function pickImage(){
+export async function pickImage(){
     let result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.All,
         allowsEditing: true,
